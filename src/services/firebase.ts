da75dd9cf07e31c4118bd72a71cf8154a4758a0b
FrancisCorp <franciscorp6969@gmail.com>
2026-07-20 15:01:30 -0500

    Agregar Firebase Storage y expo-image-picker

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,14 +1,11 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 
-import {
-  getReactNativePersistence,
-  initializeAuth,
-} from 'firebase/auth';
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { getFirestore } from 'firebase/firestore';
-
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDyIWabrvpw1T4pKDhJMF8CuCZmtVhP1Q8",
@@ -19,16 +16,14 @@ const firebaseConfig = {
   appId: "1:476590468336:web:e844c3ecf61771031a6635",
 };
 
-
 const app = initializeApp(firebaseConfig);
-
 
 export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
 });
 
-
 export const db = getFirestore(app);
 
+export const storage = getStorage(app);
 
 export default app;
